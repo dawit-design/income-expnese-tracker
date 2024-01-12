c521978d2374ed263cd2c863024cae1e19a3197b
dawit-design <dawitawegchow@gmail.com>
2024-01-12 09:51:26 -0500

    created dummy route for the users route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,41 @@ const app = express();
 
 //routes
 
+//users routes
+//POST/api/v1/users/register
+app.post('/api/v1/users/register', async (req, res) => {
+    try{
+        res.json({msg: 'Register route'})
+    }catch(error){
+        res.json(error)
+    }
+})
+//POST/api/v1/users/login
+app.post('/api/v1/users/login', async (req, res) => {
+    try{
+        res.json({msg: 'Login route'})
+    }catch(error){
+        res.json(error)
+    }
+})
+//GET/api/v1/users/profile/id
+app.get('/api/v1/users/profile/id', async (req, res) => {
+    try{
+        res.json({msg: 'Profile route'})
+    }catch(error){
+        res.json(error)
+    }
+})
+//Delete/api/v1/users/profile/id
+app.delete('/api/v1/users/profile/id', async (req, res) => {
+    try{
+        res.json({msg: 'Delete route'})
+    }catch(error){
+        res.json(error)
+    }
+})
+//account routes
+//transaction routes
 //error handling
 
 //listen to server
