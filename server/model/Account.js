675c11d4194331510f54a23ac3f2afe4523ca8ce
dawit-design <dawitawegchow@gmail.com>
2024-02-07 11:06:11 -0500

    created an account schema model

---
+++ server/model/Account.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+//account shcema
+
+const accountSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    accountType: {
+      type: String,
+      enum: [
+        "Savings",
+        "Investment",
+        "Checking",
+        "Credit Card",
+        "Builing",
+        "School",
+        "Project",
+        "Utilities",
+        "Travel",
+        "Personal",
+        "Groceries",
+        "Entertainment",
+        "Loan",
+        "Cash Flow",
+        "Uncategorized",
+      ],
+      required: true,
+    },
+    intialBalance: {
+      type: Number,
+      default: 0,
+    },
+    transactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+    createdBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    notes: {
+        type: String,
+        required: true,
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
+
+//model
+const Account = mongoose.model("User", accountSchema);
+module.exports = User;
